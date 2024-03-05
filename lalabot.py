d1758c8584e0f337dbd7976f78ba6bbbf2971678
Harry Potter <h.potter@hogwarts.prof>
2024-03-05 15:58:43 -0300

    Add more bads and improvements

--- lalabot.py
+++ lalabot.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 import discord
-from datetime import timedelta
+import datetime
 
 class Lala(discord.Client):
     async def on_ready(self):
         print("Started!!!")
-        self.bads = ["spam", "scam", "böse", "pong", "ping", "gewinnspiel", "gratis", "glücksspiel", "sapm", "masp"]
-        self.timeouts = {"karl.akemsoft.com": 1, "tyranni": 2, "schädlich": 2, "gezielt zu unterdrücken": 2}
+        self.bads = ["spam", "scam", "böse", "pong", "ping", "gewinnspiel", "gratis", "glücksspiel", "sapm", "masp", "was für ein schwachsinn", "||", "dumm"]
+        self.timeouts = {"karl.akemsoft.com": 1, "tyranni": 2, "schädlich": 2, "gezielt zu unterdrücken": 2, "I hate": 2}
         self.usersend= await client.fetch_user("1060542604573425724")
 
     async def on_message(self, message):
@@ -29,15 +29,33 @@ class Lala(discord.Client):
 
                 for bd in self.timeouts.keys():
                     if bd in message.content.lower():
-                        self.log(message.author.name + " hat eine Böse Timeout Nachricht geschrieben: " + message.content)
+                        self.log(message.author.name + " hat eine Timeout Nachricht geschrieben: " + message.content)
                         await message.delete()
-                        timeout = timedelta(hours=self.timeouts[bd])
+                        timeout = datetime.timedelta(hours=self.timeouts[bd])
                         await message.author.timeout(timeout)
                         await message.author.send("Ihre Nachricht '" + message.content + "' wurde entfernt! Wenn diese Nachricht nicht timeoutig war, sage dass @superai.ai")
+                        
+    async def on_message_edit(self, before, message):
+        self.log("Nachricht '" + before.content + "' wurde umbenannt zu " + message.content, False)
+        if message.author.name != "superai.ai" and message.channel.name != "spam-test":
+            for bd in self.bads:
+                if bd in message.content.lower():
+                    self.log(message.author.name + " hat eine Böse Nachricht geschrieben: " + message.content)
+                    await message.delete()
+                    await message.author.send("Ihre Nachricht '" + message.content + "' wurde entfernt! Wenn diese Nachricht nicht böse war, sage dass @superai.ai")
+
+            for bd in self.timeouts.keys():
+                if bd in message.content.lower():
+                    self.log(message.author.name + " hat eine Timeout Nachricht geschrieben: " + message.content)
+                    await message.delete()
+                    timeout = timedelta(hours=self.timeouts[bd])
+                    await message.author.timeout(timeout)
+                    await message.author.send("Ihre Nachricht '" + message.content + "' wurde entfernt! Wenn diese Nachricht nicht timeoutig war, sage dass @superai.ai")
                     
     def log(self, msg, pri=True):
+        msg = str(datetime.datetime.now()) + " - " + msg
         f = open("log.txt", "a")
-        f.write(msg+"/n")
+        f.write(msg+"\n\n")
         f.close()
         if pri:
             print(msg)
